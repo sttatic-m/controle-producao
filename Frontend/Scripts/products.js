bfa72c2abd05efdc751bc1db8bb1be9b10d6842e
Sttatic <sttatic2001@gmail.com>
2024-01-04 17:44:55 -0300

    Made function to fullfill table with products / Create Dynimic Edit Pages to Get a single products

--- Frontend/Scripts/products.js
+++ Frontend/Scripts/products.js
@@ -1,11 +1,36 @@
 
 let table = document.querySelector("tbody#table-body");
+getProducts();
+
+let editBtns = document.querySelectorAll("a#edit-btn");
+let editModal = document.querySelector("#edit-modal");
+editBtns.forEach(btn => {
+    
+    btn.addEventListener('shown.bs.modal', () => {
+        modal.focus();
+    });
+})
 
 async function getProducts() {
-    
+
     await fetch("http://localhost:5096/products")
-        .then(response => { response.json()})
+        .then(response => { return response.json() })
         .then(data => {
-            
+
+            data.forEach(product => {
+                table.innerHTML += `
+                    <tr>
+                        <th scope="row">${product.Code}</th>
+                        <td>${product.ProductName}</td>
+                        <td>${product.Department}</td>
+                        <td>
+                            <a class="btn btn-secondary" id="edit-btn code-${product.Code}" href="./edit.html?code=${product.Code}">Edit</a>
+                        </td>
+                        <td>
+                            <a class="btn btn-danger" id="edit-btn code-${product.Code}">Delete</a>
+                        </td>
+                    </tr>
+                `
+            });
         });
 }
