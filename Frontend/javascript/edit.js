98b02b435ca34963b01afdaf2275cda1976ba244
Sttatic <sttatic2001@gmail.com>
2024-01-05 07:36:26 -0300

    Create Edit page, made configuration to edit products

---
+++ Frontend/javascript/edit.js
@@ -0,0 +1,46 @@
+
+let params =  new URLSearchParams(window.location.search);
+let code = params.get('code');
+let form = document.querySelector("#edit-form");
+
+let codeIpt = document.querySelector("div#code-div").querySelector("input");
+let productIpt = document.querySelector("div#product-div").querySelector("input");
+let departmentIpt = document.querySelector("div#department-div").querySelector("input");
+
+getProduct();
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const data = {
+        Code: codeIpt.value,
+        ProductName: productIpt.value,
+        Department: departmentIpt.value
+    }
+
+    const options = {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }
+
+    await fetch(`http://localhost:5096/products/${code}/edit`, options)
+    .then(response => {
+        if(!response.ok) throw new Error("Request Failed - " + response.statusText);
+
+        window.location.href = "http://localhost:5500/Frontend/pages/products.html"
+    })
+    .catch(err => { window.alert(err)})
+});
+async function getProduct() {
+
+    await fetch(`http://localhost:5096/products/${code}`)
+        .then(response => { return response.json() })
+        .then(data => {
+            codeIpt.value = data.code;
+            productIpt.value = data.productName;
+            departmentIpt.value = data.department;
+        });
+}
