d905cf149ebe3599dee53c8db25fc476ce3dbb0d
Sttatic <sttatic2001@gmail.com>
2024-01-08 09:41:30 -0300

    Change API link

--- Frontend/javascript/products.js
+++ Frontend/javascript/products.js
@@ -14,7 +14,7 @@ async function getProducts() {
     const opt = {
         "cors": "no-cors"
     }
-    await fetch("https://c542-2804-d45-8c0c-d200-14f7-8bbc-f91e-4e49.ngrok-free.app/products", opt)
+    await fetch("https://3a42-2804-d45-8c0c-d200-6d9c-a15a-874c-b446.ngrok-free.app/products", opt)
         .then(response => { return response.json() })
         .then(data => {
             data.forEach(product => {
