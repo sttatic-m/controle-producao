dd6e75f62e447a6f67d3967518e9f5ebb7783380
Sttatic <sttatic2001@gmail.com>
2024-01-16 18:04:49 -0300

    Change Amount Type to Double

---
+++ ProductsAPI/Migrations/20240116210409_ChangeAmountType.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductsAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeAmountType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Amount",
+                table: "Productions",
+                type: "REAL",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Amount",
+                table: "Productions",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "REAL");
+        }
+    }
+}
--- ProductsAPI/Models/Production.cs
+++ ProductsAPI/Models/Production.cs
@@ -5,12 +5,12 @@ public class Production
     public Guid ProductionId { get; set; }
     public int Code { get; set; }
     public int ProductCode { get; set; }
-    public int Amount { get; set; }
+    public double Amount { get; set; }
     public DateTime FabricationDate { get; set; }
     public int Validity { get; set; }
     public double RecipesQuantity { get; set; }
 
-    public Production(Guid productionId, int code, int productCode, int amount, DateTime fabricationDate, int validity, double recipesQuantity)
+    public Production(Guid productionId, int code, int productCode, double amount, DateTime fabricationDate, int validity, double recipesQuantity)
     {
         ProductionId = productionId;
         Code = code;
